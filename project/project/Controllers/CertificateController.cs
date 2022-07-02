4a18d587a9184182083aa34186ea374356e6142f
SALAVOZ <salik.abdulin@mail.ru>
2022-07-02 23:12:07 +0400

    updated3

--- project/project/Controllers/CertificateController.cs
+++ project/project/Controllers/CertificateController.cs
@@ -10,15 +10,16 @@ namespace project.Controllers
     [Route("api/[controller]")]
     public class CertificateController
     {
-        private CertificateStore _certificateStore;
-        public CertificateController()
+        //private CertificateStore _certificateStore;
+        IHttpClientFactory _clientFactory;
+        public CertificateController(IHttpClientFactory clientFactory)
         {
-            
+            _clientFactory = clientFactory;
         }
 
         [HttpGet]
         [Route("selfsigned")]
-        public void createSelfSignedCertificate()
+        public async void createSelfSignedCertificate()
         {
             // Генерируем ассиметричный ключ
             var rsaKey = RSA.Create(2048);
@@ -36,15 +37,46 @@ namespace project.Controllers
             var time = DateTimeOffset.Now;
             var expirate = DateTimeOffset.Now.AddYears(5);
             var caCert = certReq.CreateSelfSigned(time, expirate);
+
+            StringBuilder builder1 = new StringBuilder();
+            builder1.AppendLine("-----BEGIN CERTIFICATE-----");
+            builder1.AppendLine(Convert.ToBase64String(caCert.RawData, Base64FormattingOptions.InsertLineBreaks));
+            builder1.AppendLine("-----END CERTIFICATE-----");
+            File.WriteAllText("//etc//nginx//ssl//caCert.crt", builder1.ToString());
+
+
             addAtStore(caCert);
-            createCertificate(caCert, time);
+            var clientCert = createCertificate(caCert, time);
+
+            SocketsHttpHandler handler = new SocketsHttpHandler();
+            X509Chain chain = new X509Chain(false);
+            handler.SslOptions.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+            {
+                //X509Chain chain = new X509Chain(false);
+                chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+                chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
+                chain.ChainPolicy.ExtraStore.Add(caCert);
+                var serverСert = new X509Certificate2(certificate);
+                return chain.Build(serverСert);
+            };
+            handler.SslOptions.ClientCertificates = new X509CertificateCollection() { clientCert };
+            HttpClient client = new HttpClient(handler);
+            var resp = await client.GetAsync("url");
         }
 
         //[HttpGet]
         //[Route("cert")]
-        public void createCertificate(X509Certificate2? caCert, DateTimeOffset signedTime)
+        public X509Certificate2? createCertificate(X509Certificate2? caCert, DateTimeOffset signedTime)
         {
             var clientKey = RSA.Create(2048);
+
+            string name = clientKey.SignatureAlgorithm.ToUpper();
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"-----BEGIN {name} PRIVATE KEY-----");
+            builder.AppendLine(Convert.ToBase64String(clientKey.ExportRSAPrivateKey(), Base64FormattingOptions.InsertLineBreaks));
+            builder.AppendLine($"-----END {name} PRIVATE KEY-----");
+            File.WriteAllText("//etc//nginx//ssl//private.key", builder.ToString());
+
             string subject = "CN=192.168.0.*";
             var clientReq = new CertificateRequest(subject, clientKey, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
@@ -58,9 +90,16 @@ namespace project.Controllers
             var expirate = signedTime.AddYears(5);
             var clientCert = clientReq.Create(caCert, signedTime, expirate, serialNumber);
 
+            StringBuilder builder1 = new StringBuilder();
+            builder1.AppendLine("-----BEGIN CERTIFICATE-----");
+            builder1.AppendLine(Convert.ToBase64String(clientCert.RawData, Base64FormattingOptions.InsertLineBreaks));
+            builder1.AppendLine("-----END CERTIFICATE-----");
+            File.WriteAllText("//etc//nginx//ssl//public.crt", builder.ToString());
+
             StorePfx(clientCert, clientKey);
             var exportCert = new X509Certificate2(clientCert.Export(X509ContentType.Cert), (string)null, X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet).CopyWithPrivateKey(clientKey);
             File.WriteAllBytes("client.pfx", exportCert.Export(X509ContentType.Pfx));
+            return clientCert;
         }
 
 
